221d9a034149701cc9d547bb2ba4af4598662426
divious1 <josehelps@gmail.com>
2019-04-12 18:57:00 -0400

    added 2 to evilginx name and update generator

--- bin/generate.py
+++ bin/generate.py
@@ -31,30 +31,100 @@ def markdown(x):
     return markdown
 
 
-def generate_savedsearches(REPO_PATH, stories):
+def generate_investigations(REPO_PATH, detections):
     # first we process detections
 
-    # detections_files = []
-    # detections_manifest_files = path.join(path.expanduser(REPO_PATH), "detections/*.json")
+    investigations = []
+    investigations_manifest_files = path.join(path.expanduser(REPO_PATH), "investigations/*.json")
+    for investigations_manifest_file in glob.glob(investigations_manifest_files):
+        # read in each story
+        try:
+            investigation = json.loads(
+                open(investigations_manifest_file, 'r').read())
+        except IOError:
+            print "ERROR: reading {0}".format(investigations_manifest_file)
+            continue
+        investigations.append(investigation)
 
-    for story_name, story in stories.iteritems():
-        if 'detections' in story:
+#    complete_investigations = dict()
+    for investigation in investigations:
+        print json.dumps(investigation, indent=4)
 
-            search_manifests = {}
-            for search_manifest_file in glob.glob(path.join(REPO_PATH, '*/detections/*.json')):
-                try:
-                    search_manifest = json.loads(open(search_manifest_file, 'r').read())
-                except Exception as e:
-                    print "Error reading %s" % search_manifest_file
-                    print e
-                    continue
 
-                search_manifests[search_manifest['search_name']] = search_manifest
+def generate_detections(REPO_PATH, stories):
+    # first we process detections
 
-    # story['mappings'] =
-    # complete_stories[story['name']]['mappings'] = {}
-    # complete_stories[story['name']]['data_models'] = set()
-    # complete_stories[story['name']]['providing_technologies'] = set()
+    detections = []
+    detections_manifest_files = path.join(path.expanduser(REPO_PATH), "detections/*.json")
+    for detections_manifest_file in glob.glob(detections_manifest_files):
+        # read in each story
+        try:
+            detection = json.loads(
+                open(detections_manifest_file, 'r').read())
+        except IOError:
+            print "ERROR: reading {0}".format(detections_manifest_file)
+            continue
+        detections.append(detection)
+
+    complete_detections = dict()
+    for detection in detections:
+
+        # lets process v1 detections
+        if detection['spec_version'] == 1:
+            if verbose:
+                print "processing v1 detection: {0}".format(detection['search_name'])
+            name = detection['search_name']
+            id = detection['search_id']
+
+            # grab search information
+            search = detection['search']
+            schedule = detection['scheduling']
+            earliest_time = schedule['earliest_time']
+            latest_time = schedule['latest_time']
+            cron = schedule['cron_schedule']
+
+        # lets process v2 detections
+        if detection['spec_version'] == 2:
+            if verbose:
+                print "processing v2 detection: {0}".format(detection['name'])
+            name = detection['name']
+            id = detection['id']
+
+            if detection['detect'] == 'splunk':
+                splunk = detection['detect']
+                search = splunk['search']
+                earliest_time = splunk['earliest_time']
+                latest_time = splunk['latest_time']
+                cron = splunk['cron']
+
+        complete_detections[name] = {}
+        complete_detections[name]['detection_name'] = name
+        complete_detections[name]['id'] = id
+        complete_detections[name]['search'] = search
+        complete_detections[name]['latest_time'] = latest_time
+        complete_detections[name]['earliest_time'] = earliest_time
+        complete_detections[name]['cron'] = cron
+
+        # collect tagging
+        metadata = detection['data_metadata']
+        if 'data_models' in metadata:
+            complete_detections[name]['data_models'] = metadata['data_models']
+        if 'providing_technologies' in metadata:
+            complete_detections[name]['providing_technologies'] = metadata['providing_technologies']
+        if 'data_source' in metadata:
+            complete_detections[name]['data_source'] = metadata['data_source']
+        complete_detections[name]['mappings'] = detection['mappings']
+        if 'fields_required' in detection:
+            complete_detections[name]['entity'] = detection['fields_required']
+
+        # stories associated with the detection
+        complete_detections[name]['stories'] = []
+        for story_name, story in sorted(stories.iteritems()):
+            for d in story['detections']:
+                if d['name'] == name:
+                    complete_detections[name]['stories'].append(story['story_name'])
+
+    return complete_detections
 
 
 def generate_analytics_story(REPO_PATH, verbose):
@@ -97,47 +167,48 @@ def generate_analytics_story(REPO_PATH, verbose):
         complete_stories[story['name']]['category'] = story['category']
         complete_stories[story['name']]['version'] = story['version']
         complete_stories[story['name']]['narrative'] = story['narrative']
+        complete_stories[story['name']]['spec_version'] = story['spec_version']
 
         # grab searches
         if story['spec_version'] == 1:
             if 'detection_searches' in story['searches']:
                 detections = []
                 for d in story['searches']['detection_searches']:
-                    detections.append({"type": "splunk", "name": "ESCU - " + d + " - Rule"})
+                    detections.append({"type": "splunk", "name": d})
                 complete_stories[story['name']]['detections'] = detections
 
             if 'support_searches' in story['searches']:
                 baselines = []
                 for b in story['searches']['support_searches']:
-                    detections.append({"type": "splunk", "name": "ESCU - " + b})
+                    detections.append({"type": "splunk", "name": b})
                 complete_stories[story['name']]['baselines'] = baselines
 
             investigations = []
             if 'contexual_searches' in story['searches']:
                 for i in story['searches']['contexual_searches']:
-                    investigations.append({"type": "splunk", "name": "ESCU - " + i})
+                    investigations.append({"type": "splunk", "name": i})
             if 'investigative_searches' in story['searches']:
                 for i in story['searches']['investigative_searches']:
-                    investigations.append({"type": "splunk", "name": "ESCU - " + i})
+                    investigations.append({"type": "splunk", "name": i})
             complete_stories[story['name']]['investigations'] = investigations
 
         if story['spec_version'] == 2:
             if 'detections' in story:
                 detections = []
                 for d in story['detections']:
-                    detections.append({"type": d['type'], "name": "ESCU - " + d['name'] + " - Rule"})
+                    detections.append({"type": d['type'], "name": d['name']})
                 complete_stories[story['name']]['detections'] = detections
 
             if 'baselines' in story:
                 baselines = []
                 for b in story['baselines']:
-                    detections.append({"type": d['type'], "name": "ESCU - " + b['name']})
+                    detections.append({"type": d['type'], "name": b['name']})
                 complete_stories[story['name']]['baselines'] = baselines
 
             if 'investigations' in story:
                 investigations = []
                 for i in story['investigations']:
-                    investigations.append({"type": i['type'], "name": "ESCU - " + i['name']})
+                    investigations.append({"type": i['type'], "name": i['name']})
             complete_stories[story['name']]['investigations'] = investigations
 
     return complete_stories
@@ -199,6 +270,8 @@ if __name__ == "__main__":
     verbose = args.verbose
 
     complete_stories = generate_analytics_story(REPO_PATH, verbose)
-    # generate_detections(REPO_PATH, complete_stories)
+    complete_detections = generate_detections(REPO_PATH, complete_stories)
+    generate_investigations(REPO_PATH, complete_detections)
+
     story_count, story_path = write_story_output(complete_stories, OUTPUT_DIR)
     print "{0} stories have been successfully to {1}".format(story_count, story_path)
