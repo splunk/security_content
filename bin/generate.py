32a797487802fcc33ab825497ffbd66839fee295
divious1 <josehelps@gmail.com>
2019-06-03 20:22:50 -0400

    corrected generator error

--- bin/generate.py
+++ bin/generate.py
@@ -255,6 +255,7 @@ def generate_investigations(REPO_PATH, detections, stories):
                     playbook_url = phantom['playbook_url']
                     earliest_time = phantom['schedule']['earliest_time']
                     latest_time = phantom['schedule']['latest_time']
+                    cron = phantom['schedule']['cron_schedule']
                     search = 'CONSTRUCT DETECTION SEARCH HERE'
                 except KeyError as e:
                     sys.exit("ERROR: \"{1}\" missing key {0} with error:\n{1}".format(e, name, e))
