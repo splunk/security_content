72d3ab875e563a02266f21f99c920c5df926cf66
bpatel <bpatel@splunk.com>
2020-05-28 13:31:49 -0700

    converted to spec3 and other updates

--- bin/generate.py
+++ bin/generate.py
@@ -57,6 +57,22 @@ def generate_transforms_conf(lookups):
 
     return output_path
 
+def generate_collections_conf(lookups):
+    filtered_lookups = list(filter(lambda i: 'collection' in i, lookups))
+    sorted_lookups = sorted(filtered_lookups, key=lambda i: i['name'])
+
+    utc_time = datetime.datetime.utcnow().replace(microsecond=0).isoformat()
+
+    j2_env = Environment(loader=FileSystemLoader('bin/jinja2_templates'),
+                         trim_blocks=True)
+    template = j2_env.get_template('collections.j2')
+    output_path = OUTPUT_PATH + "/default/collections.conf"
+    output = template.render(lookups=sorted_lookups, time=utc_time)
+    with open(output_path, 'w') as f:
+        f.write(output)
+
+    return output_path
+
 
 def generate_savedsearches_conf(detections, response_tasks, baselines, deployments):
 
@@ -505,14 +521,15 @@ if __name__ == "__main__":
     response_tasks = load_objects("response_tasks/*.yml", VERBOSE)
     deployments = load_objects("deployments/*.yml", VERBOSE)
 
-    try:
-        if VERBOSE:
-            print("generating Mitre lookups")
-        generate_mitre_lookup()
-    except:
-        print("WARNING: Generation of Mitre lookup failed.")
+    # try:
+    #     if VERBOSE:
+    #         print("generating Mitre lookups")
+    #     generate_mitre_lookup()
+    # except:
+    #     print("WARNING: Generation of Mitre lookup failed.")
 
     lookups_path = generate_transforms_conf(lookups)
+    lookups_path = generate_collections_conf(lookups)
 
     detections = sorted(detections, key=lambda d: d['name'])
     response_tasks = sorted(response_tasks, key=lambda i: i['name'])
