0fe769330771fba97f4ec9c5e38a1b801cf1364e
P4T12ICK <pbareib@splunk.com>
2020-09-09 16:16:54 +0200

    add automated detection service code

---
+++ automated_detection_testing/README.md
@@ -0,0 +1,34 @@
+# Automated Detection Testing
+Testing detection is a very important task during detection engineering. As it takes a lot of time and is a repeating task, we used the Attack Range to build an automated detection testing service.
+
+## Architecture
+![Architecture](automated_detection_testing/architecture_automated_detection_testing.png)
+The automated detection testing is using AWS Batch as execution engine. AWS batch allows you to run batch computing jobs, in our case a automated detection test. detection_service.py is the executable which controls the detection test. This executable is deployed in a docker container which is used by AWS Batch.
+
+
+## Usage
+```
+python detection_service.py
+usage: detection_service.py [-h] -tfn TEST_FILE_NAME [-arr ATTACK_RANGE_REPO]
+                            [-arb ATTACK_RANGE_BRANCH]
+                            [-scr SECURITY_CONTENT_REPO]
+                            [-scb SECURITY_CONTENT_BRANCH]
+                            [-adr ATTACK_DATA_REPO] [-adb ATTACK_DATA_BRANCH]
+                            [-gt GITHUB_TOKEN] [-smk SECRETS_MANAGER_KEY]
+detection_service.py: error: the following arguments are required: -tfn/--test_file_name
+```
+
+The detection_service.py has one mandatory parameter, which is --test_file_name. This parameter will look into the security content repository under tests for the specified test file (without extension). The other parameters are optional and can be used to specify forks of projects or specific branches. The detection_service.py is creating Pull Requests after a successful test. Therefore, it needs a Github OAUTH Token. This can be either added with the parameter --github_token or can be derived from the secrets_manager through --secrets_manager_key. Let's have a look how to use the detection service after you deployed it:
+
+Example 1:
+```
+aws batch submit-job --job-name detection_test_T1003_001 --job-definition detection_service_job --job-queue detection_service_queue --container-overrides '{"command": ["-tfn", "T1003_001"]}'
+```
+
+Example 2:
+```
+aws batch submit-job --job-name detection_test_T1003_001 --job-definition detection_service_job --job-queue detection_service_queue --container-overrides '{"command": ["-tfn", "T1003_001", "-scr", "P4T12ICK/security-content", "-scb", "develop_detection_T1003", "-smk", "github_token"]}'
+```
+
+## Deployment
+- Configure AWS Batch based on the following tutorial https://stackify.com/aws-batch-guide/ and the docker file.
---
+++ automated_detection_testing/detection_service.py
@@ -0,0 +1,259 @@
+import os
+from os import path
+import sys
+import argparse
+import git
+from shutil import copyfile
+from shutil import which
+import subprocess
+import boto3
+from random import randrange
+import yaml
+from github import Github
+from jinja2 import Environment, FileSystemLoader
+import base64
+from botocore.exceptions import ClientError
+import json
+from datetime import datetime
+
+
+
+
+def main(args):
+
+    parser = argparse.ArgumentParser(description="detection testing service based on Attack Range.")
+    parser.add_argument("-tfn", "--test_file_name", required=True,
+                        help="specify the test file name located in security content repository")
+    parser.add_argument("-arr", "--attack_range_repo", required=False, default="splunk/attack_range",
+                        help="specify the url of the atack range repository")
+    parser.add_argument("-arb", "--attack_range_branch", required=False, default="develop",
+                        help="specify the atack range branch")
+    parser.add_argument("-scr", "--security_content_repo", required=False, default="splunk/security-content",
+                        help="specify the url of the security content repository")
+    parser.add_argument("-scb", "--security_content_branch", required=False, default="develop",
+                        help="specify the security content branch")
+    parser.add_argument("-adr", "--attack_data_repo", required=False, default="splunk/attack_data",
+                        help="specify the url of the attack data repository")
+    parser.add_argument("-adb", "--attack_data_branch", required=False, default="master",
+                        help="specify the attack data branch")
+    parser.add_argument("-gt", "--github_token", required=False,
+                        help="specify the github token for the PR")
+    parser.add_argument("-smk", "--secrets_manager_key", required=False, default="github_token",
+                        help="specify the key in AWS secrets manager for your github token")
+
+
+    args = parser.parse_args()
+    test_file_name = args.test_file_name
+    attack_range_repo = args.attack_range_repo
+    attack_range_branch = args.attack_range_branch
+    security_content_repo = args.security_content_repo
+    security_content_branch = args.security_content_branch
+    attack_data_repo = args.attack_data_repo
+    attack_data_branch = args.attack_data_branch
+    github_token = args.github_token
+    secrets_manager_key = args.secrets_manager_key
+
+
+    test_obj = {}
+    test_obj['technique'] = 'T1003.001'
+    detection_obj = {}
+    detection_obj['detection'] = 'Access LSASS Memory for Dump Creation'
+    test_obj['results'] = [detection_obj]
+
+    # get github token
+    if github_token:
+        O_AUTH_TOKEN_GITHUB = github_token
+    else:
+        O_AUTH_TOKEN_GITHUB = get_secret(secrets_manager_key)
+
+    # clone repositories
+    git.Repo.clone_from('https://github.com/' + attack_range_repo, "attack_range", branch=attack_range_branch)
+    security_content_repo_obj = git.Repo.clone_from('https://' + O_AUTH_TOKEN_GITHUB + ':x-oauth-basic@github.com/' + security_content_repo, "security-content", branch=security_content_branch)
+    attack_data_repo_obj = git.Repo.clone_from('https://' + O_AUTH_TOKEN_GITHUB + ':x-oauth-basic@github.com/' + attack_data_repo, "attack_data", branch=attack_data_branch)
+
+    sys.path.append(os.path.join(os.getcwd(),'attack_range'))
+    copyfile('attack_range/attack_range.conf.template', 'attack_range/attack_range.conf')
+
+    ssh_key_name = 'ds-key-pair-' + str(randrange(10000))
+    # create ssh keys
+    ec2 = boto3.client('ec2')
+    response = ec2.create_key_pair(KeyName=ssh_key_name)
+    with open(ssh_key_name, "w") as ssh_key:
+        ssh_key.write(response['KeyMaterial'])
+    os.chmod(ssh_key_name, 0o600)
+
+
+    with open('attack_range/attack_range.conf', 'r') as file :
+      filedata = file.read()
+
+    filedata = filedata.replace('attack_range_password = Pl3ase-k1Ll-me:p', 'attack_range_password = Pl3ase-k1Ll-me:p1')
+    filedata = filedata.replace('region = us-west-2', 'region = eu-central-1')
+    filedata = filedata.replace('capture_attack_data = 0', 'capture_attack_data = 1')
+    filedata = filedata.replace('sync_to_s3_bucket = 0', 'sync_to_s3_bucket = 1')
+    filedata = filedata.replace('key_name = attack-range-key-pair', 'key_name = ' + ssh_key_name)
+    filedata = filedata.replace('private_key_path = ~/.ssh/id_rsa', 'private_key_path = /app/' + ssh_key_name)
+
+    with open('attack_range/attack_range.conf', 'w') as file:
+      file.write(filedata)
+
+    # check if terraform is installed
+    if which('terraform') is None:
+        sys.exit(1)
+    else:
+        # init terraform
+        os.system('cd attack_range/terraform && terraform init && cd ../..')
+
+    module = __import__('attack_range')
+    module.sys.argv = ['attack_range', '--config', 'attack_range/attack_range.conf', '--action', 'test', '--test_file', 'security-content/tests/' + test_file_name + '.yml']
+
+    execution_error = False
+
+    try:
+        results = module.main(module.sys.argv)
+    except Exception as e:
+        print('Error: ' + str(e))
+        module.sys.argv = ['attack_range', '--config', 'attack_range/attack_range.conf', '--action', 'destroy']
+        module.main(module.sys.argv)
+        execution_error = True
+
+    # delete ssh key
+    response = ec2.delete_key_pair(KeyName=ssh_key_name)
+
+    # check if was succesful
+    if not execution_error:
+
+        # upload results to S3
+        with open('test_results.yml', 'w') as f:
+            yaml.dump(results, f)
+        s3_client = boto3.client('s3')
+        response2 = s3_client.upload_file('test_results.yml', 'attack-range-automated-testing', results['technique'] + '/test_results.yml')
+        os.remove('test_results.yml')
+
+        # Create GitHub PR security content
+        random_number = str(randrange(10000))
+        branch_name = "automated_detection_testing_" + random_number
+        security_content_repo_obj.git.checkout(security_content_branch, b=branch_name)
+
+        for test in results['results']:
+            if not test['error']:
+                file_path = 'security-content/detections/' + test['detection'].replace(' ', '_').replace('-','_').replace('.','_').replace('/','_').lower() + '.yml'
+                detection_obj = load_file(file_path)
+                detection_obj['tags']['automated_detection_testing'] = 'passed'
+                with open(file_path, 'w') as f:
+                    yaml.dump(detection_obj, f)
+
+                changed_file_path = 'detections/' + test['detection'].replace(' ', '_').replace('-','_').replace('.','_').replace('/','_').lower() + '.yml'
+                security_content_repo_obj.index.add([changed_file_path])
+                security_content_repo_obj.index.commit('Added detection testing service results in' + test['detection'])
+
+
+        j2_env = Environment(loader=FileSystemLoader('templates'),trim_blocks=True)
+        template = j2_env.get_template('PR_template.j2')
+        body = template.render(results=results['results'])
+
+        security_content_repo_obj.git.push('--set-upstream', 'origin', branch_name)
+        g = Github(O_AUTH_TOKEN_GITHUB)
+        repo = g.get_repo("splunk/security-content")
+        pr = repo.create_pull(title="Automated Detection Testing PR " + random_number, body=body, head=branch_name, base="develop")
+
+        # Create GitHub PR attack data
+        branch_name = "automated_detection_testing_" + random_number
+        attack_data_repo_obj.git.checkout(attack_data_branch, b=branch_name)
+
+        dataset_obj = {}
+        dataset_obj['author'] = ''
+        dataset_obj['date'] = str(datetime.today().strftime('%Y-%m-%d'))
+        dataset_obj['description'] = ''
+        dataset_obj['environment'] = 'attack_range'
+        dataset_obj['technique'] = [results['technique']]
+
+        response = s3_client.list_objects_v2(
+                Bucket='attack-range-attack-data',
+                Prefix =results['technique'],
+                MaxKeys=100 )
+        dataset_urls = []
+        for dataset in response['Contents']:
+            dataset_urls.append('https://attack-range-attack-data.s3-us-west-2.amazonaws.com/' + dataset['Key'])
+
+        dataset_obj['dataset'] = dataset_urls
+        dataset_obj['references'] = ''
+        dataset_obj['sourcetypes'] = ''
+
+        folder_path = "attack_data/datasets/" + results['technique']
+        if not path.exists(folder_path):
+            os.makedirs(folder_path)
+
+        with open(folder_path + '/dataset.yml', 'w+' ) as outfile:
+	           yaml.dump(dataset_obj, outfile , default_flow_style=False, sort_keys=False)
+
+        attack_data_repo_obj.index.add(['datasets/' + results['technique'] + '/dataset.yml'])
+        attack_data_repo_obj.index.commit('Added attack data')
+
+        template = j2_env.get_template('PR_template_attack_data.j2')
+        body = template.render()
+
+        attack_data_repo_obj.git.push('--set-upstream', 'origin', branch_name)
+        g = Github(O_AUTH_TOKEN_GITHUB)
+        repo = g.get_repo("splunk/attack_data")
+        pr = repo.create_pull(title="Automated Detection Testing PR " + random_number, body=body, head=branch_name, base="master")
+
+
+
+def load_file(file_path):
+    with open(file_path, 'r') as stream:
+        try:
+            file = list(yaml.safe_load_all(stream))[0]
+        except yaml.YAMLError as exc:
+            print(exc)
+            sys.exit("ERROR: reading {0}".format(file_path))
+    return file
+
+
+def get_secret(secret_name):
+
+    region_name = "eu-central-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+            secret_obj = json.loads(secret)
+
+    return secret_obj['github_token']
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
