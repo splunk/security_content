cf69880ed0918066a829911f31ca39159090e5bc
Ignacio Bermudez Corrales <icorrales@splunk.com>
2021-02-24 12:49:18 -0800

    refactoring + assertion fix + readme

--- bin/ssa-end-to-end-testing/tests/test_ssa_detections_playground.py
+++ bin/ssa-end-to-end-testing/tests/test_ssa_detections_playground.py
@@ -5,7 +5,8 @@ import os
 import time
 
 from http import HTTPStatus
-from modules.streams_service_api_helper import compile_spl, create_pipeline_from_spl, pipeline_status, activate_pipeline, ingest_data, get_preview_id_from_spl, get_preview_data, submit_search_job, get_search_job_results, stop_preview_session, deactivate_pipeline, delete_pipeline, create_temp_index, delete_temp_index
+# from modules.streams_service_api_helper import compile_spl, create_pipeline_from_spl, pipeline_status, activate_pipeline, ingest_data, get_preview_id_from_spl, get_preview_data, submit_search_job, get_search_job_results, stop_preview_session, deactivate_pipeline, delete_pipeline, create_temp_index, delete_temp_index
+from modules.streams_service_api_helper import DSPApi
 from modules.utils import read_spl, read_data
 import pytest_check as check
 
@@ -13,9 +14,18 @@ import pytest_check as check
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 LOGGER = logging.getLogger(__name__)
 
+@pytest.fixture
+def header_token(token):
+    return f"Bearer {token}"
+
 
 @pytest.fixture
-def results_index(token):
+def api(env, tenant, header_token):
+    return DSPApi(env, tenant, header_token)
+
+
+@pytest.fixture
+def results_index(api):
     """
     Fixture that creates an temporary index, and tears it down.
 
@@ -27,92 +37,80 @@ def results_index(token):
     @return:
         Returns a descriptor of the index as a dictionary
     """
-    header_token = f"Bearer {token}"
-    temp_index = create_temp_index("playground", header_token, module="mc")
+    temp_index = api.create_temp_index("mc")
     yield temp_index
     # tear down the index
-    delete_temp_index("playground", header_token, temp_index["id"])
+    api.delete_temp_index(temp_index["id"])
 
 
-def test_data_ingestion_preview(token):
-    check.is_not_none(token, "scloud token is missing")
-    header_token = f"Bearer {token}"
-
-    ssa_detection_in_dsp_with_preview_session("playground", header_token, 'firehose.spl')
+def test_data_ingestion_preview(api):
+    ssa_detection_in_dsp_with_preview_session(api, 'firehose.spl')
 
 
-def test_data_ingestion_index(token, results_index):
-    check.is_not_none(token, "scloud token is missing")
-    header_token = f"Bearer {token}"
-
-    ssa_detection_in_dsp("playground", header_token, 'firehose2.spl', results_index)
+def test_data_ingestion_index(api, results_index):
+    ssa_detection_in_dsp(api, 'firehose2.spl', results_index)
 
 
-def test_ssa_example_detection_preview(token):
-    check.is_not_none(token, "scloud token is missing")
-    header_token = f"Bearer {token}"
-
-    ssa_detection_in_dsp_with_preview_session("playground", header_token, 'detection.spl')
+def test_ssa_example_detection_preview(api):
+    ssa_detection_in_dsp_with_preview_session(api, 'detection.spl')
 
 
-def test_ssa_example_detection(token, results_index):
-    check.is_not_none(token, "scloud token is missing")
-    header_token = f"Bearer {token}"
-
-    ssa_detection_in_dsp("playground", header_token, 'detection2.spl', results_index)
+def test_ssa_example_detection(api, results_index):
+    ssa_detection_in_dsp(api, 'detection2.spl', results_index)
 
 
 ## Helper Functions ##
 
-def ssa_detection_in_dsp_with_preview_session(env, header_token, spl):
+def ssa_detection_in_dsp_with_preview_session(api, spl):
 
-    spl = read_spl(env, spl)
+    spl = read_spl(api.env, spl)
     check.is_not_none(spl, "fail to read dummy spl file")
 
-    preview_id = get_preview_id_from_spl(env, header_token, spl)
+    preview_id = api.get_preview_id_from_spl(spl)
     check.is_not_none(preview_id, "failed to create a preview session")
 
     time.sleep(30)
 
     data = read_data(f"example.txt")
-    response_body = ingest_data(env, header_token, data)
+    response_body = api.ingest_data(data)
 
-    response, response_body = get_preview_data(env, header_token, preview_id)
+    response, response_body = api.get_preview_data(preview_id)
     check.greater(response_body.get("currentNumberOfRecords"), 0, "Missing records in preview session.")
 
-    response = stop_preview_session(env, header_token, preview_id)
+    response = api.stop_preview_session(preview_id)
 
 
-def ssa_detection_in_dsp(env, header_token, spl, results_index):
-    spl = read_spl(env, spl, results_index)
+def ssa_detection_in_dsp(api, spl, results_index):
+    spl = read_spl(api.env, spl, results_index)
     check.is_not_none(spl, "fail to read dummy spl file")
 
-    pipeline_id = create_pipeline_from_spl(env, header_token, spl)
+    pipeline_id = api.create_pipeline_from_spl(spl)
     check.is_not_none(pipeline_id, "failed to create a pipeline")
 
-    _pipeline_status = pipeline_status(env, header_token, pipeline_id)
+    _pipeline_status = api.pipeline_status(pipeline_id)
     check.equal(_pipeline_status, "CREATED", f"Current status of pipeline {pipeline_id} should be CREATED")
 
-    response_body = activate_pipeline(env, header_token, pipeline_id)
-    check.equal(response_body.get("activated"), "CREATED", f"pipeline {pipeline_id} should be successfully activate.")
+    response_body = api.activate_pipeline(pipeline_id)
+    check.equal(response_body.get("activated"), pipeline_id, f"pipeline {pipeline_id} should be successfully activate.")
 
     time.sleep(30)
 
     data = read_data(f"example.txt")
-    response_body = ingest_data(env, header_token, data)
+    response_body = api.ingest_data(data)
 
     time.sleep(30)
 
-    sid = submit_search_job(env, header_token, results_index['module'], f"from index:{results_index['name']} | search source!=\"Search Catalog\"")
+    query = f"from index:{results_index['name']} | search source!=\"Search Catalog\""
+    sid = api.submit_search_job(results_index['module'], query)
     check.is_not_none(sid, f"Failed to create a Search Job")
 
     time.sleep(30)
 
-    response_body = get_search_job_results(env, header_token, sid)
+    response_body = api.get_search_job_results(sid)
     check.greater(len(response_body.get("results")), 0, "Search job didn't return any results")
 
-    response, response_body = deactivate_pipeline(env, header_token, pipeline_id)
+    response, response_body = api.deactivate_pipeline(pipeline_id)
     check.equal(response.status_code, HTTPStatus.OK, f"The pipeline {pipeline_id} fails to deactivated.")
 
-    response = delete_pipeline(env, header_token, pipeline_id)
+    response = api.delete_pipeline(pipeline_id)
     check.equal(response.status_code, HTTPStatus.NO_CONTENT, f"Fail to delete pipeline {pipeline_id}.")
