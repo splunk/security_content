61dcd8a94bc80efdc9799c2c32567ed7b5c24e60
Ignacio Bermudez Corrales <icorrales@splunk.com>
2021-03-05 18:41:07 -0800

    running version

--- bin/ssa-end-to-end-testing/smoke-test-runner/entrypoint.sh
+++ bin/ssa-end-to-end-testing/smoke-test-runner/entrypoint.sh
@@ -2,7 +2,7 @@
 
 export VAULT_TOKEN=`cat /vault/.vault-token`
 
-SCLOUD_TOKEN=$(vault read  -format json $SMOKETEST_VAULT_READ_PATH --tenant=$TENANT | jq -r '.data.token')
+SCLOUD_TOKEN=$(vault read  -format json $SMOKETEST_VAULT_READ_PATH tenant=$TENANT | jq -r '.data.token')
 
 git clone https://github.com/splunk/security_content.git && cd security_content && git checkout $SRCBRANCH
 
@@ -10,4 +10,4 @@ cd bin/ssa-end-to-end-testing
 
 virtualenv -p python3 smoketest && source smoketest/bin/activate && pip3 install -r requirements.txt
 
-python run_ssa_smoketest.py -t $SCLOUD_TOKEN -e $DSP_ENV -s $TENANT
+python run_ssa_smoketest.py -t $SCLOUD_TOKEN -e $DSP_ENV -s $TENANT -b $SRCBRANCH
