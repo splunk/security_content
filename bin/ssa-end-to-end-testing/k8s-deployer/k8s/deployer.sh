27ada7b1076d399c358bfb94be0094e460fac779
peter-cg <682716+peter-cg@users.noreply.github.com>
2021-07-26 16:42:17 -0500

    qbec env vars

--- bin/ssa-end-to-end-testing/k8s-deployer/k8s/deployer.sh
+++ bin/ssa-end-to-end-testing/k8s-deployer/k8s/deployer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "qbec show"
-qbec show $SCSENV -c service-account -c smoketest
+qbec show $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest
 
 echo "qbec validate"
-qbec validate $SCSENV -c service-account -c smoketest
+qbec validate $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest
 
 echo "qbec apply"
 qbec --yes apply $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest --wait
