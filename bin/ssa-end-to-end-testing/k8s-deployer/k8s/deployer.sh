d8957ba06e8b0d2873fb73db5f01c065450efbd3
peter-cg <682716+peter-cg@users.noreply.github.com>
2021-07-27 11:47:13 -0500

    Modify qbec verbosity

--- bin/ssa-end-to-end-testing/k8s-deployer/k8s/deployer.sh
+++ bin/ssa-end-to-end-testing/k8s-deployer/k8s/deployer.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "qbec show"
+export
+
+echo "$ qbec show $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest"
 qbec show $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest
 
-echo "qbec validate"
+echo "$ qbec validate $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest"
 qbec validate $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest
 
-echo "qbec apply"
+echo "$ qbec --yes apply $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest --wait"
 qbec --yes apply $SCSENV --vm:ext-str SRCBRANCH --vm:ext-str CI_COMMIT_SHORT_SHA --vm:ext-str CI_JOB_ID --vm:ext-str SMOKETEST_RUNNER_IMAGE -c service-account -c smoketest --wait
