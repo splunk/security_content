80a1b9ad9b4095dafb880440685b35291f5064f4
pyth0n1c <87383215+pyth0n1c@users.noreply.github.com>
2021-11-16 17:19:43 -0800

    More progress adding and parsing new arguments.

--- automated_detection_testing/ci/detection_testing_batch/new_arguments.py
+++ automated_detection_testing/ci/detection_testing_batch/new_arguments.py
@@ -3,6 +3,9 @@ import sys
 
 
 def main(args):
+
+    default_args = {}
+
     parser = argparse.ArgumentParser(
         description="Use 'SOME_PROGRAM_NAME_STRING --help' to get help with the arguments")
     actions_parser = parser.add_subparsers(title="test action")
@@ -21,10 +24,41 @@ def main(args):
     test_parser.add_argument(
         '-pr', '--pull_request_number', required=False, help="Pull request number.")
 
+    VALID_DETECTION_TYPES = ['endpoint', 'cloud', 'network']
+
+    #Common Test Arguments
+    test_parser.add_argument('-t', '--types', type=str, action="append", 
+                             help="Detection types to test. Can be one or more of %s"%(VALID_DETECTION_TYPES))
+    
+    
+    test_parser.add_argument('-e', '--escu_package', type=argparse.FileType('rb'), required=False, 
+                                        help="A previously generated ESCU PAcklage to use.  If you pass this "\
+                                             "argument, a new ESCU package will not be generated.  Note that this "\
+                                             "may cause newly-written detections to fail (for example, if they "\
+                                             "leverage macros that have been added or modified).")
+   
+    test_parser.add_argument('-p','--persist_security_content', required=False, action="store_true",
+                             help="Assumes security_content directory already exists.  Don't check it out and overwrite it again.  Saves "\
+                             "time and allows you to test a detection that you've updated.  Runs generate again in case you have "\
+                             "updated macros or anything else.  Especially useful for quick, local, iterative testing.")
+
+
+    test_parser.add_argument('tag', '--container_tag', required=False, default = default_args['container_tag'], 
+                             help="The tag of the Splunk Container to use.  Tags are located "\
+                                  "at https://hub.docker.com/r/splunk/splunk/tags")
+
+    test_parser.add_argument("-show", "--show_password", required=False, default=False, action='store_true', 
+                             help="Show the generated password to use to login to splunk.  For a CI/CD run, "\
+                            "you probably don't want this.")
+
+    
+    #Mode settings
     mode_parser = test_parser.add_subparsers(title="Test Modes", required=True)
-    new_parser = mode_parser.add_parser("new", 
-                                        #aliases=['changed'], 
+    #NEW
+    new_parser = mode_parser.add_parser("changes", 
                                         help="Test only the new or changed detections")
+
+    #SELECTED
     selected_parser = mode_parser.add_parser("selected", help="Test only the detections from the target branch that "\
                                                               " are passed on the command line.  These can be given as "\
                                                               "a list of files or as a file containing a list of files.")
@@ -35,16 +69,16 @@ def main(args):
                                 required=False, help="The names of files that you want to test, separated by commas.  "\
                                                      "Do not include spaces between the detections!")
     
-
-
-    
+    #ALL
     all_parser = mode_parser.add_parser("all", 
-                                        #aliases=['everything'], 
                                         help="Test all of the detections in the target branch.  "\
                                              "Note that this could take a very long time.")
 
 
-    parser.parse_args()
+    a = parser.parse_args()
+    
+    print(a)
+    print(a.__dict__)
 
 
 if __name__ == "__main__":
