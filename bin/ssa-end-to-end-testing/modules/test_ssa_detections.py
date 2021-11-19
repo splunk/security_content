4d3280992315457000635a0d65d3615103d81a49
P4T12ICK <pbareiss@splunk.com>
2021-11-19 09:58:55 +0100

    improved testing

--- bin/ssa-end-to-end-testing/modules/test_ssa_detections.py
+++ bin/ssa-end-to-end-testing/modules/test_ssa_detections.py
@@ -162,7 +162,7 @@ class SSADetectionTesting:
 
         while not (search_results or max_execution_time_reached):
             max_execution_time_reached = self.wait_time(WAIT_CYCLE)
-            query = f"from indexes('{self.results_index['name']}') | search source!=\"Search Catalog\" "
+            query = f"from indexes('detection_testing') | search test_id=\"{test_id}\" "
             sid = self.api.submit_search_job(self.results_index['module'], query)
             assert sid is not None, f"Failed to create a Search Job"
 
