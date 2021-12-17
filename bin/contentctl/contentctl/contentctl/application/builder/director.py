422a0f5b9d4989b1f11a165c8e65ae94e0a05483
P4T12ICK <pbareiss@splunk.com>
2021-12-17 18:08:12 +0100

    add builder design pattern instead of repository

---
+++ bin/contentctl/contentctl/contentctl/application/builder/director.py
@@ -0,0 +1,11 @@
+
+
+from contentctl.contentctl.application.builder.builder import Builder
+
+class SecurityContentDirector():
+    
+    def constructTTPDetections(builder: Builder):
+        pass
+
+    def constructAnomalyDetections(builder: Builder):
+        pass
