5c1e44eb3254a0ac70056dbc5b19a1512209f837
patel-bhavin <bhavin.j.patel91@gmail.com>
2022-06-27 13:49:44 -0700

    adding more technologieas

--- bin/contentctl_project/contentctl_infrastructure/builder/security_content_detection_builder.py
+++ bin/contentctl_project/contentctl_infrastructure/builder/security_content_detection_builder.py
@@ -91,9 +91,13 @@ class SecurityContentDetectionBuilder(DetectionBuilder):
 
     def addProvidingTechnologies(self) -> None:
         if self.security_content_obj:
-            if self.security_content_obj.tags.supported_tas:
-                if 'Splunk_TA_microsoft_sysmon' in self.security_content_obj.tags.supported_tas:
-                    self.security_content_obj.providing_technologies = ['Sysmon','Microsoft Windows']
+            # if self.security_content_obj.tags.supported_tas:
+                if 'Endpoint' in self.security_content_obj.datamodel:
+                    self.security_content_obj.providing_technologies = "['Sysmon', 'Microsoft Windows','Carbon Black Response','CrowdStrike Falcon', 'Symantec Endpoint Protection']"
+                if "`cloudtrail`" in str(self.security_content_obj.search):
+                    self.security_content_obj.providing_technologies = "['Amazon Web Services - Cloudtrail']"
+                if '`wineventlog_security`' in self.security_content_obj.search or '`powershell`' in self.security_content_obj.search:
+                    self.security_content_obj.providing_technologies = "['Microsoft Windows']"
 
 
     def addNesFields(self) -> None:
