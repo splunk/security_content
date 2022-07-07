80af56d3970c5472eef981e44bc7af9e822c4dcc
pyth0n1c <87383215+pyth0n1c@users.noreply.github.com>
2022-07-07 14:33:57 -0700

    Fixing the rare case where enrichment fails due to CVESearch API failure, causing the entire workflow to fail.  Now, we will retry enrichment up to a total of 3 times. This should virtually eliminate all CVE Enrichment failures. If they do still occur, we will receive a descriptive message indicating why there was a failure instead of the cryptic message that we were getting before.

--- bin/contentctl_project/contentctl_infrastructure/builder/cve_enrichment.py
+++ bin/contentctl_project/contentctl_infrastructure/builder/cve_enrichment.py
@@ -11,8 +11,13 @@ CVE_CACHE_FILENAME = "lookups/CVE_CACHE.db"
 
 NON_PERSISTENT_CACHE = {}
 
+
+
 @functools.cache
-def cvesearch_helper(url:str, cve_id:str, force_cached_or_offline:bool=False):
+def cvesearch_helper(url:str, cve_id:str, force_cached_or_offline:bool=False, max_api_attempts:int=3):
+    if max_api_attempts < 1:
+            raise(Exception(f"The minimum number of CVESearch API attempts is 1.  You have passed {max_api_attempts}"))
+
     if force_cached_or_offline:
         if not os.path.exists(CVE_CACHE_FILENAME):
             print(f"Cache at {CVE_CACHE_FILENAME} not found - Creating it.")
@@ -23,14 +28,27 @@ def cvesearch_helper(url:str, cve_id:str, force_cached_or_offline:bool=False):
         result = cache[cve_id]
         #print(f"hit cve_enrichment:  {time.time() - start:.2f}")
     else:
-        try:
-            cve = cvesearch_id_helper(url)
-            result = cve.id(cve_id)
-        except Exception as e:
-            raise(Exception(f"The option 'force_cached_or_offline' was used, but {cve_id} not found in {CVE_CACHE_FILENAME} and unable to connect to {CVESSEARCH_API_URL}"))
+        api_attempts_remaining = max_api_attempts
+        while api_attempts_remaining > 0:
+            
+            api_attempts_remaining -= 1
+            
+            start = time.time()
+            try:
+                cve = cvesearch_id_helper(url)
+                result = cve.id(cve_id)
+                break
+            except Exception as e:
+                if api_attempts_remaining > 0:
+                    print(f"The option 'force_cached_or_offline' was used, but {cve_id} not found in {CVE_CACHE_FILENAME} and unable to connect to {CVESSEARCH_API_URL}: {str(e)}")
+                    print(f"Retrying the CVESearch API up to {api_attempts_remaining} more times...")
+                else:
+                    raise(Exception(f"The option 'force_cached_or_offline' was used, but {cve_id} not found in {CVE_CACHE_FILENAME} and unable to connect to {CVESSEARCH_API_URL} after {max_api_attempts} attempts: {str(e)}"))
+            
         if result is None:
             raise(Exception(f'CveEnrichment for [ {cve_id} ] failed - CVE does not exist'))
         cache[cve_id] = result
+        
     if force_cached_or_offline:
         cache.close()
 
@@ -52,7 +70,7 @@ class CveEnrichment():
         cve_enriched = dict()
         try:
             
-            result = cvesearch_helper(CVESSEARCH_API_URL, cve_id)
+            result = cvesearch_helper(CVESSEARCH_API_URL, cve_id, force_cached_or_offline)
             cve_enriched['id'] = cve_id
             cve_enriched['cvss'] = result['cvss']
             cve_enriched['summary'] = result['summary']
