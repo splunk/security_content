d7c52ee6e2acc7233d5ffb19cad703b74d1f7f6f
pyth0n1c <87383215+pyth0n1c@users.noreply.github.com>
2022-09-29 15:48:23 -0700

    Initial changes to validate the path in the linked dataset

--- bin/contentctl_project/contentctl_core/domain/entities/unit_test_attack_data.py
+++ bin/contentctl_project/contentctl_core/domain/entities/unit_test_attack_data.py
@@ -9,3 +9,24 @@ class UnitTestAttackData(BaseModel):
     source: str
     sourcetype: str = None
     update_timestamp: bool = None
+
+    @validator('data')
+    def data_valid(cls, v):
+        VALID_PREAMBLES = ["https://media.githubusercontent.com/media/splunk/attack_data/master/"]
+        for preamble in VALID_PREAMBLES:
+            if v.startswith(preamble):
+                return v
+
+        #If we get this far, then the dataset did not exist with a valid preamble    
+        valid_paths = '\n\t'.join(VALID_PREAMBLES)
+        raise ValueError(f"Dataset link {v} does not being with a valid path. Valid paths are: \n\t{valid_paths}")
+    
+    '''
+    @validator('file_name')
+    def file_name_valid(cls, v, values):
+        filename_from_data = values['data'].split('/')[-1]
+        if v != filename_from_data:
+            print(f"AttackData Source Field {values['data']} has an expected filename of {filename_from_data}, however the file_name field provided was {v}")
+            #raise ValueError(f"AttackData Source Field {values['data']} has an expected filename of {filename_from_data}, however the file_name field provided was {v}")
+        return v
+    '''
