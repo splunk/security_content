0c72ce5f21498d2a28b982c48e63fd1f7c940272
d1vious <josehelps@gmail.com>
2022-10-18 10:05:42 -0400

    fixing syntax issues with the search, now they are passing

--- bin/lolba_enrichment/lolbas_enrichment.py
+++ bin/lolba_enrichment/lolbas_enrichment.py
@@ -57,7 +57,7 @@ def write_ba_detections(lolbas, TEMPLATE_PATH, VERBOSE, OUTPUT_PATH):
         full_ssa_search = ''
         # windows_lolbin_binary_in_non_standard_path auto search generation
         # first process SSA search
-        ssa_base_search = 'ssa_input = | from read_ssa_enriched_events() | eval device=ucast(map_get(input_event, "dest_device_id"), "string", null), user=ucast(map_get(input_event, "dest_user_id"), "string", null), timestamp=parse_long(ucast(map_get(input_event, "_time"), "string", null)), process_name=lower(ucast(map_get(input_event, "process_name"), "string", null)), process_path=lower(ucast(map_get(input_event, "process_path"), "string", null)), event_id=ucast(map_get(input_event, "event_id"), "string", null)'
+        ssa_base_search = '| from read_ssa_enriched_events() | eval device=ucast(map_get(input_event, "dest_device_id"), "string", null), user=ucast(map_get(input_event, "dest_user_id"), "string", null), timestamp=parse_long(ucast(map_get(input_event, "_time"), "string", null)), process_name=lower(ucast(map_get(input_event, "process_name"), "string", null)), process_path=lower(ucast(map_get(input_event, "process_path"), "string", null)), event_id=ucast(map_get(input_event, "event_id"), "string", null)'
         ssa_end_search ='| eval start_time=timestamp,end_time=timestamp, entities=mvappend(device, user), body=create_map(["event_id", event_id, "process_path", process_path, "process_name", process_name]) | into write_ssa_detected_events();'
         condition_1 = '| where process_name IS NOT NULL AND '
         condition_2 = '| where process_path IS NOT NULL AND '
