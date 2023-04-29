b1688fd2ecf211ceb1cbbab3528a5d8c4407e74e
Kelby Shelton <kelbys@splunk.com>
2023-04-29 16:43:09 -0500

    Made required changes. See description.
    Playbook is failing when only some reputation results were returned.
    
    These are the changes I made, since we don't want to delay this by waiting for another attack range:
    1. Added a filter block after "filter successful observable output."
    2. Adding the filter negates the need to do a custom code block before the format. Removed custom code.
    3. Rename "filter successful observable output" to "observable output decision," because its not actually filtering. Its just a decision.
    4. Reword "observable output of detonation playbooks not exits" to "No observable output exists for dispatched playbooks."
    5. Switched beginning decision to filter and routed the filtered artifact IDs to the playbook.

--- playbooks/Dynamic_Analysis_Dispatch.py
+++ playbooks/Dynamic_Analysis_Dispatch.py
@@ -12,45 +12,25 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'decision_1' block
-    decision_1(container=container)
+    # call 'new_artifacts_filter' block
+    new_artifacts_filter(container=container)
 
     return
-
-@phantom.playbook_block()
-def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("decision_1() called")
-
-    # check for 'if' condition 1
-    found_match_1 = phantom.decision(
-        container=container,
-        logical_operator="or",
-        conditions=[
-            ["artifact:*.cef.requestURL", "!=", ""],
-            ["artifact:*.cef.vaultId", "!=", ""]
-        ])
-
-    # call connected blocks if condition 1 matched
-    if found_match_1:
-        dispatch_detonation_playbooks(action=action, success=success, container=container, results=results, handle=handle)
-        return
-
-    # check for 'else' condition 2
-    artifacts_check_comment(action=action, success=success, container=container, results=results, handle=handle)
-
-    return
-
 
 @phantom.playbook_block()
 def dispatch_detonation_playbooks(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("dispatch_detonation_playbooks() called")
 
+    filtered_artifact_0_data_new_artifacts_filter = phantom.collect2(container=container, datapath=["filtered-data:new_artifacts_filter:condition_1:artifact:*.id"])
+
+    filtered_artifact_0__id = [item[0] for item in filtered_artifact_0_data_new_artifacts_filter]
+
     inputs = {
-        "playbook_repo": [],
         "playbook_tags": ["sandbox"],
-        "artifact_ids_include": [],
-        "indicator_tags_exclude": [],
+        "playbook_repo": [],
         "indicator_tags_include": [],
+        "indicator_tags_exclude": [],
+        "artifact_ids_include": filtered_artifact_0__id,
     }
 
     ################################################################################
@@ -109,11 +89,11 @@ def filter_successful_observable_output(action=None, success=None, container=Non
 
     # call connected blocks if condition 1 matched
     if found_match_1:
-        normalized_observables_report(action=action, success=success, container=container, results=results, handle=handle)
+        filter_dispatch_output(action=action, success=success, container=container, results=results, handle=handle)
         return
 
     # check for 'else' condition 2
-    detonations_output_check_comment(action=action, success=success, container=container, results=results, handle=handle)
+    no_observables_comment(action=action, success=success, container=container, results=results, handle=handle)
 
     return
 
@@ -126,11 +106,14 @@ def merge_report_1(action=None, success=None, container=None, results=None, hand
     # summary report for all sandbox detonation input playbooks.
     ################################################################################
 
-    template = """SOAR retrieved tickets from Splunk. The table below shows a summary of the information gathered.\\n\\n\n| value | Score | Confidence | Source |\n| --- | --- | --- | --- |\n%%\n{0}\n%%"""
+    template = """SOAR retrieved tickets from Splunk. The table below shows a summary of the information gathered.\n\n| Value | Score | Confidence | Source |\n| --- | --- | --- | --- |\n%%\n| {0} | {1} | {2} | {3} |\n%%"""
 
     # parameter list for template variable replacement
     parameters = [
-        "normalized_observables_report:custom_function:sandbox_merge_report"
+        "filtered-data:filter_dispatch_output:condition_1:dispatch_detonation_playbooks:playbook_output:observable.value",
+        "filtered-data:filter_dispatch_output:condition_1:dispatch_detonation_playbooks:playbook_output:observable.reputation.score",
+        "filtered-data:filter_dispatch_output:condition_1:dispatch_detonation_playbooks:playbook_output:observable.reputation.confidence",
+        "filtered-data:filter_dispatch_output:condition_1:dispatch_detonation_playbooks:playbook_output:observable.source"
     ]
 
     ################################################################################
@@ -151,73 +134,20 @@ def merge_report_1(action=None, success=None, container=None, results=None, hand
 
 
 @phantom.playbook_block()
-def normalized_observables_report(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("normalized_observables_report() called")
-
-    ################################################################################
-    # This block uses custom code for normalizing observable result of input playbooks 
-    # tag as sandbox. The normalized output will be used in merging 4 different types 
-    # of sandbox used to detonated url or vault_id.
-    ################################################################################
-
-    dispatch_detonation_playbooks_output_observable = phantom.collect2(container=container, datapath=["dispatch_detonation_playbooks:playbook_output:observable"])
-
-    dispatch_detonation_playbooks_output_observable_values = [item[0] for item in dispatch_detonation_playbooks_output_observable]
-
-    normalized_observables_report__sandbox_merge_report = None
-    normalized_observables_report__observable_value = None
-    normalized_observables_report__observable_type = None
-
-    ################################################################################
-    ## Custom Code Start
-    ################################################################################
-
-    # Write your custom code here...
-    normalized_observables_report__sandbox_merge_report = []
-
-    normalized_observables_report__observable_value = []
-    normalized_observables_report__observable_type = []
-    format_observable = ""
-    output_observable_values =  [(i or "") for i in dispatch_detonation_playbooks_output_observable_values] 
-    
-    for observable_item in output_observable_values:
-        if isinstance(observable_item, dict):
-            normalized_observables_report__observable_value.append(observable_item['value'])
-            normalized_observables_report__observable_type.append(observable_item['type'])
-            format_observable += "{} | {} | {} | {} \n".format(observable_item['value'], observable_item['reputation']['score'], observable_item['reputation']['confidence'], observable_item['source'])
-    normalized_observables_report__sandbox_merge_report.append(format_observable)
-    
-    #phantom.debug("{} len:{}".format(normalized_observables_report__sandbox_merge_report, len(normalized_observables_report__sandbox_merge_report)))
-    normalized_observables_report__observable_value = normalized_observables_report__observable_value[0]
-    normalized_observables_report__observable_type = normalized_observables_report__observable_type[0]
-
-    ################################################################################
-    ## Custom Code End
-    ################################################################################
-
-    phantom.save_run_data(key="normalized_observables_report:sandbox_merge_report", value=json.dumps(normalized_observables_report__sandbox_merge_report))
-    phantom.save_run_data(key="normalized_observables_report:observable_value", value=json.dumps(normalized_observables_report__observable_value))
-    phantom.save_run_data(key="normalized_observables_report:observable_type", value=json.dumps(normalized_observables_report__observable_type))
-
-    merge_report_1(container=container)
-
-    return
-
-
-@phantom.playbook_block()
 def indicator_tag(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
     phantom.debug("indicator_tag() called")
 
-    normalized_observables_report__observable_type = json.loads(_ if (_ := phantom.get_run_data(key="normalized_observables_report:observable_type")) != "" else "null")  # pylint: disable=used-before-assignment
-    normalized_observables_report__observable_value = json.loads(_ if (_ := phantom.get_run_data(key="normalized_observables_report:observable_value")) != "" else "null")  # pylint: disable=used-before-assignment
+    filtered_output_0_dispatch_detonation_playbooks_output_observable = phantom.collect2(container=container, datapath=["filtered-data:filter_dispatch_output:condition_1:dispatch_detonation_playbooks:playbook_output:observable.classifications","filtered-data:filter_dispatch_output:condition_1:dispatch_detonation_playbooks:playbook_output:observable.value"])
 
     parameters = []
 
-    parameters.append({
-        "tags": normalized_observables_report__observable_type,
-        "indicator": normalized_observables_report__observable_value,
-        "overwrite": None,
-    })
+    # build parameters list for 'indicator_tag' call
+    for filtered_output_0_dispatch_detonation_playbooks_output_observable_item in filtered_output_0_dispatch_detonation_playbooks_output_observable:
+        parameters.append({
+            "tags": filtered_output_0_dispatch_detonation_playbooks_output_observable_item[0],
+            "indicator": filtered_output_0_dispatch_detonation_playbooks_output_observable_item[1],
+            "overwrite": None,
+        })
 
     ################################################################################
     ## Custom Code Start
@@ -268,11 +198,11 @@ def workbook_update_task(action=None, success=None, container=None, results=None
 
 
 @phantom.playbook_block()
-def detonations_output_check_comment(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("detonations_output_check_comment() called")
+def no_observables_comment(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("no_observables_comment() called")
 
     ################################################################################
-    # observable output of detonation playbooks not exits.
+    # observable output of detonation playbooks does not exist
     ################################################################################
 
     ################################################################################
@@ -285,7 +215,65 @@ def detonations_output_check_comment(action=None, success=None, container=None, 
     ## Custom Code End
     ################################################################################
 
-    phantom.comment(container=container, comment="observable output of detonation playbooks not exits")
+    phantom.comment(container=container, comment="No observable output found for dispatched playbooks.")
+
+    return
+
+
+@phantom.playbook_block()
+def filter_dispatch_output(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("filter_dispatch_output() called")
+
+    ################################################################################
+    # Filter out any results that are Null.
+    ################################################################################
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["dispatch_detonation_playbooks:playbook_output:observable", "!=", ""]
+        ],
+        name="filter_dispatch_output:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        merge_report_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    return
+
+
+@phantom.playbook_block()
+def new_artifacts_filter(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("new_artifacts_filter() called")
+
+    ################################################################################
+    # This block is used to ensure only "new" artifacts are utilized.
+    ################################################################################
+
+    # collect filtered artifact ids and results for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.id", "!=", ""]
+        ],
+        name="new_artifacts_filter:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        dispatch_detonation_playbooks(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
+
+    # collect filtered artifact ids and results for 'if' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.id", "==", ""]
+        ],
+        name="new_artifacts_filter:condition_2")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_2 or matched_results_2:
+        artifacts_check_comment(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_2, filtered_results=matched_results_2)
 
     return
 
