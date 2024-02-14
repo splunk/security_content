9fdf182033d637838bd9a15ad004c9e07b950445
Eric McGinnis <emcginnis@splunk.com>
2024-02-14 23:54:00 +0000

    Release v4.24.0

--- bin/contentctl_project/contentctl_core/application/adapter/adapter.py
+++
@@ -1,9 +0,0 @@
-import abc
-
-from bin.contentctl_project.contentctl_core.domain.entities.enums.enums import SecurityContentType
-
-class Adapter(abc.ABC):
-
-    @abc.abstractmethod
-    def writeObjects(self, objects: list, output_path: str, type: SecurityContentType = None) -> None:
-        pass
--- bin/contentctl_project/contentctl_infrastructure/tests/adapter/obj_to_md_data_ref/_pages/endpoint.md
+++
@@ -1,9 +0,0 @@
----
-title: Endpoint
-layout: tag
-author_profile: false
-taxonomy: Endpoint
-permalink: /detections/endpoint/
-sidebar:
-  nav: "detections"
----
