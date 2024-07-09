5ec9d4c7bb8877a414ddfc39bee8d24953f6c277
Bhavin Patel <bhavin.j.patel91@gmail.com>
2024-07-09 08:54:01 -0700

    warnings

--- .github/workflows/format_test_results.py
+++ .github/workflows/format_test_results.py
@@ -4,18 +4,28 @@ A simple script formatting test_results/summary.yml to display on github actions
 
 import yaml
 import re
+import os
 
 def main():
+
+    # Define the path to the YAML file
+    yaml_file_path = 'test_results/summary.yml'
+   
+    # Check if the YAML file exists
+    if not os.path.exists(yaml_file_path):
+        print(f"Error: The file {yaml_file_path} does not exist.")
+        exit(1)  # Exit with an error code
+
     # Load the YAML file
-    with open('/home/runner/work/security_content/security_content/test_results/summary.yml', 'r') as file:
+    with open(yaml_file_path, 'r') as file:
         data = yaml.safe_load(file)
     
     # Extract total_fail value and debug print it
     total_fail = data['summary']['total_fail']
-    print(f"Extracted total_fail: [{total_fail}]")
+    print(f"Extracted total_fail: [{total_fail}]\n")
     
     # Print all unit test details first
-    print("Unit Test Details:")
+    print("Unit Test Details:\n")
     print(f"{'Name':<80} | {'Status':<6} | {'Test Type':<10} | {'Exception':<50}")
     print(f"{'----':<80} | {'------':<6} | {'---------':<10} | {'---------':<50}")
     for detection in data['tested_detections']:
@@ -32,11 +42,14 @@ def main():
 
     # Check if total_fail is a valid integer and greater than or equal to one
     if re.match(r'^[0-9]+$', str(total_fail)) and int(total_fail) >= 1:
-        print("CI Failure: There are failed tests.")
+        # Print the message in bold
+        print("\033[1m\nCI Failure: There are failed tests.\n\033[0m")
+        print("\033[1mDownload the job artifacts of this run and view complete summary in test_results/summary.yml for troubleshooting failures.\n\033[0m") 
         exit(1)  # Fail the CI job
     else:
         print("CI Success: No failed tests.\n\n")
 
-    print("Download the job artifacts of this run and view complete summary in test_results/summary.yml for troubleshooting failures .\n")
+    
+    
 if __name__ == "__main__":
     main()
